ca77921042d12ce7d81463cc3d8be3ab2d9b0cba
Espartaco Palma <esparta@gmail.com>
2020-04-30 19:25:59 -0700

    Identifying our users via cookies
    How about the user is returning to the site?
    We can relate who it was just by setting the cookie.
    So, as long is the same browser we can, at some point
    get their history or broadcast them.

--- app/controllers/preview_controller.rb
+++ app/controllers/preview_controller.rb
@@ -2,10 +2,20 @@
 
 # Our main Controller
 class PreviewController < ApplicationController
+  before_action :assign_user_id
+
   def show
   end
 
   def create
     render json: { ack: SecureRandom.hex }
+  end
+
+  private
+
+  def assign_user_id
+    return if cookies[:user_id]
+
+    cookies[:user_id] = SecureRandom.hex
   end
 end
