3b3a523d6a86faadd2aeec244d9ff24811195be6
Espartaco Palma <esparta@gmail.com>
2020-05-03 12:11:42 -0700

    Using Transaction object in the ActiveJob
    In this last piece we are moving all the logic from PreviewJob
    to the already introduced and test Transaction object.
    
    The Transaction class is in charge on how and what we are doing
    in order to get the process done, leaving the workflow
    more more clear and focused, the other components as
    Controller, Model and ActiveJobs has their responsabilities
    and are tested according.

--- app/jobs/previewer_job.rb
+++ app/jobs/previewer_job.rb
@@ -6,37 +6,7 @@ class PreviewerJob < ApplicationJob
   queue_as :default
 
   def perform(uri:, user_id:)
-    Url.transaction do
-      url = Url.create(
-        uri: uri, user_id: user_id,
-        acknowledge_id: job_id, started_at: DateTime.now
-      )
-      og = Downloader::OpenGraph.get(url.uri)
-      if og.failure?
-        url.error!
-      else
-        url.parsing!
-
-        # Create their images
-        url.downloading!
-        results = og.value!.images.map do |image_url|
-          image = Downloader.get(image_url)
-          if image.success?
-            url.url_images.create(uri: image_url).tap do |new_image|
-              image = Downloader.get(image_url)
-              if image.success?
-                new_image.image.attach(
-                  io: image.value!,
-                  filename: 'image.jpg'
-                )
-              end
-            end
-          else
-            false
-          end
-        end
-        results.all? ? url.ready! : url.error!
-      end
-    end
+    transaction = Transaction.call(uri, user_id, job_id)
+    Rails.logger.error(transaction.failure) if transaction.failure?
   end
 end
